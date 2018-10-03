1fa035f53fb2545d56a6b8847ea520eda9877a27
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-10-03 16:06:25 +0300

    Feature: Bind Event data to the event details activity - Create a viewmodel to handle the data  called from the API - Bind the data from the viewmodel to the event details activity

--- app/src/main/java/com/events/events/eventapplication/EventDetailsActivity.java
+++ app/src/main/java/com/events/events/eventapplication/EventDetailsActivity.java
@@ -1,17 +1,31 @@
 package com.events.events.eventapplication;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.events.events.eventapplication.databinding.ActivityEventDetailsBinding;
+import com.events.events.eventapplication.models.Event;
 
 public class EventDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityEventDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_event_details);
-        binding.setEvent(getIntent().getStringExtra("eventId"));
+        final ActivityEventDetailsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_event_details);
+//        Event event = new Event("beach", "Entebbe", "things about to go down");
+
+
+        EventDetailsViewModel eventDetailsViewModel = ViewModelProviders.of(this).get(EventDetailsViewModel.class);
+        eventDetailsViewModel.getEvent(Integer.parseInt(getIntent().getStringExtra("eventId"))).observe(this, new Observer<Event>() {
+            @Override
+            public void onChanged(@Nullable Event event) {
+                binding.setEvent(event);
+            }
+        });
     }
 }
--- app/src/main/java/com/events/events/eventapplication/EventDetailsViewModel.java
+++ app/src/main/java/com/events/events/eventapplication/EventDetailsViewModel.java
@@ -1,4 +1,40 @@
 package com.events.events.eventapplication;
 
-public class EventDetailsViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+
+import com.events.events.eventapplication.models.Event;
+
+public class EventDetailsViewModel extends ViewModel {
+
+    private MutableLiveData<Event> eventLiveData;
+
+    public LiveData<Event> getEvent(int eventId){
+        if(eventLiveData == null){
+            eventLiveData = new MutableLiveData<>();
+            loadEvent(eventId);
+        }
+
+        return eventLiveData;
+    }
+
+    public void loadEvent(int eventId){
+        EventAsyncTask eventAsyncTask = new EventAsyncTask();
+        eventAsyncTask.execute(eventId);
+    }
+
+    private class EventAsyncTask extends AsyncTask<Integer, Void, Event> {
+        @Override
+        protected Event doInBackground(Integer... eventId) {
+            return ApiCalls.getEvent(eventId[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            super.onPostExecute(event);
+            eventLiveData.setValue(event);
+        }
+    }
 }
