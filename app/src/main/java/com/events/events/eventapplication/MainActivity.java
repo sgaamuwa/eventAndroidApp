d29a460540e8a693ac704f58889c712c864876e3
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-10-05 16:57:46 +0300

    Feature: Refactor Main Activity to use a FrameLayout - Remove the Event Fragment and replace with a FrameLayout - Set up listener for the bottom menu to change fragments with click

--- app/src/main/java/com/events/events/eventapplication/MainActivity.java
+++ app/src/main/java/com/events/events/eventapplication/MainActivity.java
@@ -1,7 +1,11 @@
 package com.events.events.eventapplication;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // load the Event List Fragment as the initial fragment
+        loadFragment(new EventListFragment());
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.friends_tab:
+                    fragment = new UserListFragment();
+                    break;
+                case R.id.events_tab:
+                    fragment = new EventListFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+        }
+    };
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
