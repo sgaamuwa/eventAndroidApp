5188cb23f0ca40a6906ec1bfae5f52966812f9af
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-10-07 11:44:14 +0300

    Task: Add Retrofit to the project

---
+++ app/src/main/java/com/events/events/eventapplication/UserViewModel.java
@@ -0,0 +1,4 @@
+package com.events.events.eventapplication;
+
+public class UserViewModel {
+}
---
+++ app/src/main/java/com/events/events/eventapplication/rest/EventsAPIService.java
@@ -0,0 +1,4 @@
+package com.events.events.eventapplication.rest;
+
+public class EventsAPIService {
+}
