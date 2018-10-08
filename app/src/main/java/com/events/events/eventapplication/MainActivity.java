8f7e955cbf2c1f8b3f0d3e4cf8e49777486d9037
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-10-08 14:57:24 +0300

    Feature: Add a custom Toolbar for the main activity - Add menu icons - Improve look of the user list fragment view

--- app/src/main/java/com/events/events/eventapplication/MainActivity.java
+++ app/src/main/java/com/events/events/eventapplication/MainActivity.java
@@ -5,20 +5,34 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTopToolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(mTopToolbar);
+
         // load the Event List Fragment as the initial fragment
         loadFragment(new EventListFragment());
 
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.navigation_bar);
         bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
