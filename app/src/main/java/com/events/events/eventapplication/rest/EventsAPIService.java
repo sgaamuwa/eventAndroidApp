1297f6755c3e18b69f037c5b7b53fb143d175439
Samuel Gaamuwa <samuel.gaamuwa@andela.com>
2018-10-08 14:52:16 +0300

    Feature: Create interface to associate with API calls for the event API

--- app/src/main/java/com/events/events/eventapplication/rest/EventsAPIService.java
+++ app/src/main/java/com/events/events/eventapplication/rest/EventsAPIService.java
@@ -1,4 +1,24 @@
 package com.events.events.eventapplication.rest;
 
-public class EventsAPIService {
+import com.events.events.eventapplication.models.Event;
+import com.events.events.eventapplication.models.User;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface EventsAPIService {
+
+    String BASEURL = "https://981cdbce-68d6-465c-8f08-d7e91f51c223.mock.pstmn.io/";
+
+    @GET("events/")
+    Call<List<Event>> listEvents();
+
+    @GET("events/{event}")
+    Call<Event> listEvent(@Path("event") int eventId);
+
+    @GET("users")
+    Call<List<User>> listUsers();
 }
